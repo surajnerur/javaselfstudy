466f65e6ab9558d32e926bf0b34f9cb74baa93c6
Suraj Nerurkar <suraj.nerurkar@tieto.com>
2019-05-06 14:21:05 +0530

    updated by Suraj

---
+++ javastreams/src/main/java/javastreams/practice/cs/Person.java
@@ -0,0 +1,50 @@
+package javastreams.practice.cs;
+
+public class Person {
+
+	private String name;
+
+	private int age;
+
+	private double salary;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Person(String name, int age, double salary) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", salary=" + salary + "]";
+	}
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
---
+++ javastreams/src/main/java/javastreams/practice/cs/StreamsImpl.java
@@ -0,0 +1,99 @@
+package javastreams.practice.cs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamsImpl {
+
+	static List<Person> listPerson = new ArrayList<Person>();
+
+	static {
+		listPerson.add(new Person("abcd", 23,1000));
+		listPerson.add(new Person("defh", 35,2000));
+		listPerson.add(new Person("ijkl", 63,1400));
+		listPerson.add(new Person("lmno", 53,1040));
+		listPerson.add(new Person("opqr", 53,1001));
+		listPerson.add(new Person("rstu", 52,1006));
+		listPerson.add(new Person("uvwx", 26,1007));
+		listPerson.add(new Person("xyza", 46,1600));
+		listPerson.add(new Person("bcde", 75,1700));
+		listPerson.add(new Person("fghi", 36,1040));
+		listPerson.add(new Person("jklm", 45,2000));
+		listPerson.add(new Person("mnop", 35,2030));
+		listPerson.add(new Person("qrst", 63,1030));
+		listPerson.add(new Person("uvwx", 65,1200));
+		listPerson.add(new Person("yzab", 17,3000));
+		listPerson.add(new Person("cdef", 75,4090));
+		listPerson.add(new Person("ghij", 63,5007));
+		listPerson.add(new Person("klmn", 25,1004));
+		listPerson.add(new Person("opqr", 37,1009));
+		listPerson.add(new Person("stuv", 44,1003));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("-----------PRINT ALL---------------");
+		
+		listPerson.stream().filter(p -> p.getAge() > 55).forEach(p -> System.out.println(p));
+		
+		System.out.println("-----------List to MAP ---------------");
+		
+		Map<String, Integer> map = listPerson.stream().filter(p -> p.getAge() > 55).collect(Collectors.toMap(Person::getName, Person::getAge));
+		System.out.println(map);
+		
+		System.out.println("-----------PRINT using MergeFunction---------------");
+		
+		Map<Integer, String> map2 = listPerson.stream().filter(p -> p.getAge() > 55)
+				.collect(Collectors.toMap(Person::getAge, Person::getName, 
+						(oldValue, newValue) -> {
+							//return both values seperated by ":"
+							return oldValue + ":" + newValue;
+						}
+				));
+		
+		System.out.println(map2);
+		
+		System.out.println("-----------Get Various Map Data Structures ---------------");
+		
+		TreeMap<Integer, String> collectedMap = listPerson.stream().filter(p -> p.getAge() > 55).collect(Collectors.toMap(Person::getAge, Person::getName, (v1, v2) -> v1 + ":" + v2, TreeMap::new));
+		
+		System.out.println(collectedMap);
+		
+		System.out.println("-----------Merge 2 Data Structure---------------");
+		
+		List<Person> collect = listPerson.stream().filter(p -> p.getAge() > 55).collect(Collectors.toList());
+		
+		List<Person> list2 = Arrays.asList(
+				new Person("Harshal", 32, 3434343), 
+				new Person("Suraj", 35, 232343));		
+		
+		Stream<Person> mergedStream = Stream.concat(list2.stream(), collect.stream());
+		
+		mergedStream.forEach(System.out::println);
+		
+		System.out.println("-----------Calculate Total Salary with Conditions ---------------");
+
+		double sumOfAllSalary = listPerson.stream().filter(p -> p.getAge() > 55).mapToDouble(p -> p.getSalary()).sum();
+		
+		System.out.println(sumOfAllSalary);
+		
+		System.out.println("-----------Calculate Total Salary with Conditions - Type 2---------------");
+		
+		//double sumOfAllSalary = listPerson.stream().filter(p -> p.getAge() > 55).reduce
+		
+		System.out.println("-----------PRINT ALL---------------");
+		System.out.println("-----------PRINT ALL---------------");
+
+	}
+
+}
